931c99840f3804f9f0e7bf433d6d845814d6cfaf
Esmaeelipoor Jahromi <dp7924@kit.edu>
2025-08-05 20:14:28 +0200

    inputs of executable files are documented based on NISTIR

--- simulations/01_linux.py
+++ simulations/01_linux.py
@@ -27,27 +27,53 @@ disrealnew_path = os.path.join(bin_path, "disrealnew")
 
 # === Input for genpartnew ===
 genpartnew_input = "\n".join([
-    "-3034", "2", "16", "0", "0.0604", "0.515 0.041",
-    "1", "17", "1", "1", "15", "1", "1", "14", "1", "1", "13", "1", "2", "12", "1",
-    "2", "11", "1", "4", "10", "1", "5", "9", "1", "8", "8", "1", "13", "7", "1",
-    "21", "6", "1", "38", "5", "1", "73", "4", "1", "174", "3", "1", "450", "2", "1",
-    "2674", "1", "1", "4", "3", "1", "8",
-    os.path.join(src_path, "cem140w04floc.img"),
-    os.path.join(src_path, "pcem140w04floc.img"),
-    "1"
+    "-3034",  # Random number seed
+    "2",  # Menu choice to place particles
+    "16",  # Number of size classes to place
+    "0",  # Dispersion distance between particles in pixels
+    "0.0604",  # Calcium sulfate (total) volume fraction
+    "0.515 0.041",  # Fractions of calcium sulfate that are hemihydrate and anhydrite
+    "1", "17", "1",  # Size class 1: Number, Radius, Phase ID (1 = cement)
+    "1", "15", "1",  # Size class 2
+    "1", "14", "1",  # Size class 3
+    "1", "13", "1",  # Size class 4
+    "2", "12", "1",  # Size class 5
+    "2", "11", "1",  # Size class 6
+    "4", "10", "1",  # Size class 7
+    "5", "9", "1",  # Size class 8
+    "8", "8", "1",  # Size class 9
+    "13", "7", "1",  # Size class 10
+    "21", "6", "1",  # Size class 11
+    "38", "5", "1",  # Size class 12
+    "73", "4", "1",  # Size class 13
+    "174", "3", "1",  # Size class 14
+    "450", "2", "1",  # Size class 15
+    "2674", "1", "1",  # Size class 16
+    "4",  # Menu selection to report phase counts
+    "3",  # Menu selection to flocculate particles
+    "1",  # Number of separate flocs (particle clusters) to create
+    "8",  # Menu selection to output current microstructure to file
+    os.path.join(src_path, "cem140w04floc.img"), # Filename to save image to
+    os.path.join(src_path, "pcem140w04floc.img"), # Filename to save particle IDs to
+    "1" # Menu selection to exit program (end)
 ])
 
-# Run the genpartnew executable
 
 
 # === Input for distrib3d ===
 distrib3d_input = "\n".join([
-    "-99",
-    os.path.join(src_path, "cem140w04floc.img"),
-    "cement140",
-    os.path.join(src_path, "cement140w04flocf.img"),
-    "0.7344", "0.6869", "0.0938", "0.1337",
-    "0.1311", "0.1386", "0.0407", "0.0408"
+    "-99", # Random number seed
+    os.path.join(src_path, "cem140w04floc.img"), # Filename of original microstructure image
+    "cement140",  # File root name for correlation filters (sil, c3s, c3a, etc.)
+    os.path.join(src_path, "cement140w04flocf.img"), # Filename under which to save final microstructure
+    "0.7344",  # Volume fraction of C3S in microstructure to be created
+    "0.6869",  # Surface area fraction of C3S in microstructure to be created
+    "0.0938",  # Volume fraction of C2S in microstructure to be created
+    "0.1337",  # Surface area fraction of C2S in microstructure to be created
+    "0.1311",  # Volume fraction of C3A in microstructure to be created
+    "0.1386",  # Surface area fraction of C3A in microstructure to be created
+    "0.0407",  # Volume fraction of C4AF in microstructure to be created
+    "0.0408"   # Surface area fraction of C4AF in microstructure to be created
 ]) + "\n"
 
 
@@ -63,29 +89,55 @@ img2 = os.path.join(src_path, "pcem140w04floc.img")
 
 # === Prepare input data ===
 disrealnew_input = "\n".join([
-    "-2794",
-    img1,
-    "1 2 3 4 5 6 7 28 26",
-    "35",
-    img2,
-    "44990", "1",
-    "5850", "2",
-    "8692", "3",
-    "2631", "4",
-    "1100", "5",
-    "2062", "6",
-    "839", "7",
-    "0",
-    "1000", "0", "500",
-    "0.0001 9000.",
-    "0.01 9000.",
-    "0.00002 10000.",
-    "0.002 2500.",
-    "50", "5", "5000", "100",
-    "0.00", "20.0", "20.0", "0.0",
-    "40.0", "83.14", "80.0", "0.00035",
-    "0.72", "0", "0", "1", "10",
-    "1.0", "0", "0", "1"
+    "-2794", # Random number seed
+    img1, 
+    # Filename containing input 3-D phase ID microstructure
+    "1 2 3 4 5 6 7 28 26",  # Phase assignments for C3S, C2S, etc.
+    "35",  # Phase ID for C3A in fly ash particles
+    img2, # Filename containing input 3-D particle ID microstructure
+    "44990",  # Number of one-pixel particles to add
+    "1",  # Add one-pixel particles of phase C3S
+    "5850",  # Number of one-pixel particles to add
+    "2",  # Add one-pixel particles of phase C2S
+    "8692",  # Number of one-pixel particles to add
+    "3",  # Add one-pixel particles of phase C3A
+    "2631",  # Number of one-pixel particles to add
+    "4",  # Add one-pixel particles of phase C4AF
+    "1100",  # Number of one-pixel particles to add
+    "5",  # Add one-pixel particles of gypsum
+    "2062",  # Number of one-pixel particles to add
+    "6",  # Add one-pixel particles of hemihydrate
+    "839",  # Number of one-pixel particles to add
+    "7",  # Add one-pixel particles of anhydrite
+    "0",  # Number of one-pixel particles to add
+    "1000",  # Number of cycles of hydration model to execute
+    "0",  # Flag for executing model under saturated conditions
+    "500",  # Maximum number of diffusion steps per cycle
+    "0.0001 9000.",  # Nucleation parameters for CH
+    "0.01 9000.",  # Nucleation parameters for gypsum (dihydrate)
+    "0.00002 10000.",  # Nucleation parameters for C3AH6
+    "0.002 2500.",  # Nucleation parameters for FH3
+    "50",  # Cycle frequency for checking pore space percolation
+    "5",  # Cycle frequency for checking total solids percolation
+    "5000",  # Cycle frequency for outputting particle hydration stats
+    "100",  # Cycle frequency for outputting hydrated microstructures
+    "0.00",  # Induction time in hours
+    "20.0",  # Initial specimen temperature in degrees Celsius
+    "20.0",  # Ambient temperature in degrees Celsius
+    "0.0",  # Overall heat transfer coefficient
+    "40.0",  # Activation energy for cement hydration
+    "83.14",  # Activation energy for pozzolanic reactions
+    "80.0",  # Activation energy for slag hydration
+    "0.00035",  # Conversion factor to go from cycles to real time
+    "0.72",  # Aggregate volume fraction in actual concrete mixture
+    "0",  # Flag indicating that hydration is under isothermal conditions
+    "0",  # Flag indicating that C-S-H conversion is prohibited
+    "1",  # Flag indicating that CH/aggregate precipitation is allowed
+    "10",  # Number of slices to include in a hydration movie file
+    "1.0",  # One-voxel dissolution bias factor
+    "0",  # Number of cycles to execute before resaturation
+    "0",  # Flag indicating that C-S-H morphology is random
+    "1"   # Flag indicating that pH does influence hydration kinetics
 ]) + "\n"
 
 
